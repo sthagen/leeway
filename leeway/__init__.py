e16f88d300b69d46ca914d27373009ad16f72d35
Stefan Hagen <stefan@hagen.link>
2023-10-22 13:57:46 +0200

    Bumped impl version for release 2023.10.22

--- leeway/__init__.py
+++ leeway/__init__.py
@@ -1,6 +1,6 @@
 # [[[fill git_describe()]]]
-__version__ = '2022.12.13+parent.ff7049ef'
-# [[[end]]] (checksum: 853a9f21e62834d23aa0601ad62a61ce)
+__version__ = '2023.10.22+parent.25b2f774'
+# [[[end]]] (checksum: af4497b40adbd7cbf7116732d6106bcd)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
